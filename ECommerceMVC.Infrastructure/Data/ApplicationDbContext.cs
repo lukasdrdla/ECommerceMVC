84c92933fa4737c83068923dcfe17c24ced8dedf
Drdla <drdla@gmail.com>
2025-07-18 05:45:08 +0200

    create dbcontext

---
+++ ECommerceMVC.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,48 @@
+﻿using ECommerceMVC.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceMVC.Infrastructure.Data;
+
+public class ApplicationDbContext : IdentityDbContext<AppUser, AppRole, string>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Brand> Brands { get; set; } = null!;
+    public DbSet<Cart> Carts { get; set; } = null!;
+    public DbSet<CartItem> CartItems { get; set; } = null!;
+    public DbSet<Category> Categories { get; set; } = null!;
+    public DbSet<Inventory> Inventories { get; set; } = null!;
+    public DbSet<Order> Orders { get; set; } = null!;
+    public DbSet<OrderItem> OrderItems { get; set; } = null!;
+    public DbSet<Payment> Payments { get; set; } = null!;
+    public DbSet<Product> Products { get; set; } = null!;
+    public DbSet<ProductImage> ProductImages { get; set; } = null!;
+    public DbSet<ProductTag> ProductTags { get; set; } = null!;
+    public DbSet<ProductVariant> ProductVariants { get; set; } = null!;
+    public DbSet<ProductVariantAttribute> ProductVariantAttributes { get; set; } = null!;
+    public DbSet<ProductVariantImage> ProductVariantImages { get; set; } = null!;
+    public DbSet<RefundRequest> RefundRequests { get; set; } = null!;
+    public DbSet<Review> Reviews { get; set; } = null!;
+    public DbSet<ShippingMethod> ShippingMethods { get; set; } = null!;
+    public DbSet<Tag> Tags { get; set; } = null!;
+    public DbSet<Warehouse> Warehouses { get; set; } = null!;
+    public DbSet<Wishlist> Wishlists { get; set; } = null!;
+    public DbSet<WishlistItem> WishlistItems { get; set; } = null!;
+    
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        //seed data
+    }
+    
+    
+    
+    
+    
+}
